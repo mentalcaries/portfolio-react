8e7de8c95af63fed4ccea1e9e636c9d56f2c48bf
Devin Jaggernauth <neexis@gmail.com>
2022-02-01 00:11:59 -0400

    hero, menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,47 @@
-import React from "react";
-import profileLogo from '../../images/dj-logo.png'
+import React from 'react';
+import profileLogo from '../../images/dj-logo.png';
 
-function Header(){
-  return(
-<header class="flex justify-between w-full my-6 px-6" >
-  <img src={profileLogo} alt="DJ Profile" />
-  <nav>
-  <ul class="flex gap-6">
-            <li class="navbar__item"><a href="#about" class="navbar__link hover-animate">About</a></li>
-            <li class="navbar__item"><a href="#projects" class="navbar__link hover-animate">Projects</a></li>
-            <li class="navbar__item"><a href="#experience" class="navbar__link hover-animate">Experience</a></li>
-            <li class="navbar__item"><a href="#contact" class="navbar__link hover-animate">Contact</a></li>
-          </ul>
-        </nav>
-</header>
-  )
+function Header() {
+  return (
+    <header class="flex justify-between max-w-screen-x mt-6 px-10">
+      <img src={profileLogo} alt="DJ Profile" />
+      <nav class="flex items-center">
+        <ul class="flex gap-8">
+          <li class="">
+            <a
+              href="#about"
+              class="font-sans text-lg uppercase font-semibold tracking-wider hover:opacity-60 ease-linear duration-300"
+            >
+              About
+            </a>
+          </li>
+          <li class="navbar__item">
+            <a
+              href="#projects"
+              class="font-sans text-lg uppercase font-semibold hover:opacity-60 ease-linear duration-300"
+            >
+              Projects
+            </a>
+          </li>
+          <li class="navbar__item">
+            <a
+              href="#experience"
+              class="font-sans text-lg uppercase font-semibold tracking-wider tracking-wider hover:opacity-60 ease-linear duration-300"
+            >
+              Experience
+            </a>
+          </li>
+          <li class="navbar__item">
+            <a
+              href="#contact"
+              class="font-sans text-lg uppercase font-semibold tracking-wider hover:opacity-60 ease-linear duration-300"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
-export default Header
+export default Header;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,12 @@
 
 function Main(){
   return(
-    <section class="bg-hero-image h-[785px] w-full">
-
+    <section class="bg-hero-image bg-center back min-h-[640px] w-full -mt-12 flex items-center">
+          <div class=" max-w-screen-md m-28">
+            <h1 class="font-sans font-bold text-4xl">Devin Jaggernauth</h1>
+            <p class="font-sans font-medium text-xl">Full Stack Web Developer</p>
+            <p class="font-sans font-medium text-xl">DDS</p>
+          </div>
     </section>
   )
 }
