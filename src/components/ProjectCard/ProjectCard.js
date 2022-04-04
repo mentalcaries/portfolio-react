7836933409a44d7f24b264dfd3dee9d60c754cc4
Devin Jaggernauth <neexis@gmail.com>
2022-04-03 21:00:15 -0400

    project card remove tailwind

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ProjectCard.css';
 import cardImage from '../../images/around.png';
 
 function ProjectCard() {
@@ -7,7 +8,7 @@ function ProjectCard() {
     // Overlay
     // Text
     // Links to code and deployment
-    <article className="shadow-lg rounded-lg p-5">
+    <article className="project-card">
       <a
         href="https://mentalcaries.students.nomoreparties.site"
         target="_blank"
@@ -17,17 +18,19 @@ function ProjectCard() {
           className="
         "
         >
-          <img
-            src={cardImage}
-            alt="Project Card"
-            className="max-h-80 w-auto rounded-xl object-contain hover:scale-[1.02] duration-300"
-          />
-          <p className="text-center">A full stack photo gallery app that allows users add, delete or like photos. Registration and authentication handled on the back end  with photos and users saved to a Mongo database</p>
+          <img src={cardImage} alt="Project Card" className="project__image" />
+          <h3 className="project__title">
+            Around the U.S
+          </h3>
+          <p className="project__desc">
+            A full stack photo gallery app that allows users add, delete or like
+            photos. Registration and authentication handled on the back end with
+            photos and users saved to a Mongo database
+          </p>
         </div>
       </a>
-      <h3 className="font-semibold text-xl mt-4 mb-1 text-center">Around the U.S</h3>
-      <p className="text-center">
-        React | Express | Node | MongoDB | Google Cloud
+      <p className="project__tech">
+        React | Node | MongoDB | Google Cloud
       </p>
     </article>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,21 +7,11 @@ import git from '../images/git.png';
 import mongo from '../images/mongo.png';
 import node from '../images/node.png';
 import figma from '../images/figma.png';
-import photoshop from '../images/ps.png';
-
-// const iconArray = [
-//   ['Javascript', javascript],
-//   ['React', react],
-//   ['Node', node],
-//   ['Express', express],
-//   ['MongoDB', mongo],
-//   ['Git', git],
-//   ['HTML', html],
-//   ['CSS', css],
-//   ['Figma', figma],
-//   ['Photoshop', photoshop],
-
-// ]
+import sql from '../images/sql.png';
+import newsxp from '../images/newsxp.png';
+import around from '../images/around.png';
+import dotcot from '../images/dotcot.png';
+import elise from '../images/elise.png';
 
 const iconArray = [
   {name: 'Javascript', icon: javascript},
@@ -29,11 +19,48 @@ const iconArray = [
   {name: 'Node', icon: node},
   {name: 'Express', icon: express},
   {name: 'MongoDB', icon: mongo},
+  {name: 'SQL', icon: sql},
   {name: 'Git', icon: git},
   {name: 'HTML', icon: html},
   {name: 'CSS', icon: css},
   {name: 'Figma', icon: figma},
-  {name: 'Photoshop', icon: photoshop},
 ];
 
-export default iconArray;
+const projectCards = [
+  {
+    url: 'https://newsxp.students.nomoreparties.sbs/',
+    image: newsxp,
+    title: 'News Explorer',
+    desc: `This is a fully responsive single page application that allows users to search for news articles by keyword. It also features user registration and login, which allows for saving of articles for later reading, plus user authentication and session management using security best practices.`,
+    tech: 'React | Node | MongoDB | Google Cloud',
+  },
+  {
+    url: 'https://mentalcaries.students.nomoreparties.site',
+    image: around,
+    title: 'Around the US',
+    desc: `A full stack photo gallery app that allows users add, delete or like
+  photos . Registration and authentication handled on the back end with
+  photos and users being saved to a Mongo database`,
+    tech: 'React | Node | MongoDB | Google Cloud',
+  },
+  {
+    url: 'https://peaceful-woodland-39661.herokuapp.com/',
+    image: dotcot,
+    title: 'DotCot',
+    desc: `A full stack photo gallery app that allows users add, delete or like
+  photos . Registration and authentication handled on the back end with
+  photos and users being saved to a Mongo database`,
+    tech: 'React | Node | MongoDB | Heroku',
+  },
+  {
+    url: 'https://mentalcaries.students.nomoreparties.site',
+    image: around,
+    title: 'Around the US',
+    desc: `A full stack photo gallery app that allows users add, delete or like
+  photos . Registration and authentication handled on the back end with
+  photos and users being saved to a Mongo database`,
+    tech: 'React | Node | MongoDB | Google Cloud',
+  },
+];
+
+export default {iconArray, projectCards};
