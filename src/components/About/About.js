1e46edc189a4bfa351c07dde83210756f0618866
Devin Jaggernauth <neexis@gmail.com>
2022-04-09 10:53:48 -0400

    update about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './About.css';
 import profile from '../../images/profile.jpg';
 import {motion} from 'framer-motion';
+import { aboutMe } from '../../utils/constants';
 
 function About() {
   return (
@@ -15,11 +16,7 @@ function About() {
       <div className="about__content">
         <div className="about__text">
           <h2 className="about__title">About Me</h2>
-          {[
-            "Hi, I'm Devin, a Full Stack Developer who's thrilled by the endless possibilities that come with code!",
-            "I'm from a background in healthcare and small business management, and my knack for problem solving has made this journey equally fun and challenging.",
-            'I believe that the small stuff matters, so attention to detail is always key, whether it comes to clinical practice or writing code.',
-          ].map((paragraph) => (
+          {aboutMe.map((paragraph) => (
             <p className="about__paragraph" key={Math.random()}>
               {paragraph}
             </p>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -23,6 +23,12 @@ import mongoicon from '../images/mongo96.png';
 import gcloudicon from '../images/gcloud96.png';
 import herokuicon from '../images/heroku96.png';
 
+const aboutMe = [
+  "Hi, I'm Devin, a Full Stack Developer who's thrilled by the endless possibilities that come with code!",
+  "Despite my initial apprehension, my knack for problem solving led me to dip my toes in programming, and I've had no regrets since. I can safely say this journey had been equally fun and challenging, and has pushed me to keep learning as much as I can",
+  'I come from a background in healthcare where the small stuff matters; attention to detail is always key, whether it comes to clinical practice or writing code.',
+]
+
 const iconArray = [
   {name: 'Javascript', icon: javascript},
   {name: 'React', icon: react},
@@ -72,8 +78,8 @@ const projectCards = [
     key: 103,
     url: 'https://mentalcaries.github.io/code-jam-july-landing/index.html',
     image: elise,
-    title: 'Portfolio Page - Elise',
-    desc: `This project presents a clean and modern landing page coded in pure HTML and CSS, and features responsive behaviour down to mobile sizes.`,
+    title: 'Elise Bouer - Portfolio',
+    desc: `This Portfolio presents a clean, modern and responsive landing page coded in pure HTML and CSS. Submitted for Practicum's July 2021 Code Jam for team overReact !`,
     tech: [htmlicon, cssicon],
     frontend: 'https://github.com/mentalcaries/code-jam-july-landing',
   },
@@ -97,4 +103,4 @@ const contacts = [
   },
 ];
 
-export {iconArray, projectCards, contacts};
+export {aboutMe, iconArray, projectCards, contacts};
